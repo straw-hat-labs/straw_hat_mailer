e17e2543ae16e6eddc702dfab0606e92afe74583
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-09-12 11:11:04 -0700

    update test

--- test/straw_hat_mailer_test/email_test.exs
+++ test/straw_hat_mailer_test/email_test.exs
@@ -5,12 +5,29 @@ defmodule StrawHat.Mailer.Test.EmailTest do
 
   test "email with template" do
     template = insert(:template)
-    assert %Swoosh.Email{} = email =  Email.template(template.name, %{name: "jristo", number: "1 000 000"})
+    opts = %{
+      name: "jristo",
+      number: "1 000 000"
+    }
+    assert %Swoosh.Email{} = email =  Email.template(template.name, opts)
+    assert email.html_body == "<b>Become </b> our client number <i>1 000 000</i>, enjoy the service."
+    assert email.text_body == "Welcome jristo, enjoy a good reputation"
+  end
+
+  test "email with template and struct data" do
+    template = insert(:template, %{text_body: "Welcome {{account.username}}, enjoy a good reputation"})
+    opts = %{
+      number: "1 000 000",
+      account: %{
+        username: "jristo"
+      }
+    }
+    assert %Swoosh.Email{} = email =  Email.template(template.name, opts)
     assert email.html_body == "<b>Become </b> our client number <i>1 000 000</i>, enjoy the service."
     assert email.text_body == "Welcome jristo, enjoy a good reputation"
   end
 
   test "email without template" do
-    assert %Swoosh.Email{} = Email.template("fake_name", %{name: "jristo", number: "1 000 000"})
+    assert %Swoosh.Email{} = Email.template("fake_name", %{})
   end
 end
