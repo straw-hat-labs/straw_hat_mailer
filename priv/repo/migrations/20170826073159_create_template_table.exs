8c9d02222cc179d32d2f45a35f1294bdfd657e99
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-09-12 16:25:06 -0700

    update migrations

--- priv/repo/migrations/20170826073159_create_template_table.exs
+++ priv/repo/migrations/20170826073159_create_template_table.exs
@@ -4,13 +4,14 @@ defmodule StrawHat.Mailer.Repo.Migrations.CreateTemplateTable do
   def change do
     create table(:templates) do
       add(:name, :string, null: false)
-      add(:service, :string, null: false)
-      add(:from, :map, null: false)
+      add(:title, :string, null: false)
       add(:subject, :string, null: false)
-      add(:text_body, :string, null: false)
+      add(:owner_id, :string, null: false)
+      add(:privacy, :string, default: "private")
+      add(:text_body, :string)
       add(:html_body, :string)
     end
 
-    create index(:templates, [:name], unique: true)
+    create index(:templates, [:owner_id, :name], unique: true)
   end
 end
