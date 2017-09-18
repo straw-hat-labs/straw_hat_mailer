76ba1d79585435058a82d2e92ffeb232974ce539
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-09-17 22:43:58 -0700

    update email test

--- test/straw_hat_mailer_test/email_test.exs
+++ test/straw_hat_mailer_test/email_test.exs
@@ -1,33 +1,70 @@
 defmodule StrawHat.Mailer.Test.EmailTest do
   use StrawHat.Mailer.Test.DataCase, async: true
 
-  alias StrawHat.Mailer.Email
+  import StrawHat.Mailer.Email
 
   test "email with template" do
     template = insert(:template)
-    opts = %{
+    options = %{
       name: "jristo",
       number: "1 000 000"
     }
-    assert %Swoosh.Email{} = email =  Email.template(template.name, opts)
+    email =
+      new_email(
+        to: "john@gmail.com",
+        from: "support@myapp.com",
+        template: template.name,
+        options: options
+      )
+
     assert email.html_body == "<b>Become </b> our client number <i>1 000 000</i>, enjoy the service."
     assert email.text_body == "Welcome jristo, enjoy a good reputation"
   end
 
   test "email with template and struct data" do
     template = insert(:template, %{text_body: "Welcome {{account.username}}, enjoy a good reputation"})
-    opts = %{
+    options = %{
       number: "1 000 000",
       account: %{
         username: "jristo"
       }
     }
-    assert %Swoosh.Email{} = email =  Email.template(template.name, opts)
+    email =
+      new_email(
+        to: "john@gmail.com",
+        from: "support@myapp.com",
+        template: template.name,
+        options: options
+      )
+
     assert email.html_body == "<b>Become </b> our client number <i>1 000 000</i>, enjoy the service."
     assert email.text_body == "Welcome jristo, enjoy a good reputation"
   end
 
   test "email without template" do
-    assert %Swoosh.Email{} = Email.template("fake_name", %{})
+    assert %Swoosh.Email{} =
+      new_email(
+        to: "john@gmail.com",
+        from: "support@myapp.com",
+        subject: "Welcome john"
+      )
+  end
+
+  test "send email" do
+    assert {:ok, %{id: _}} =
+      new_email(
+        to: "john@gmail.com",
+        from: "support@myapp.com",
+        subject: "Welcome john"
+      ) |> send()
+  end
+
+  test "send email later" do
+    assert {:ok, _} =
+      new_email(
+        to: "john@gmail.com",
+        from: "support@myapp.com",
+        subject: "Welcome john"
+      ) |> send_later()
   end
 end
