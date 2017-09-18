5ce05f6a42f3a6eb24058804c1f37fe001cb88af
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-09-17 22:44:35 -0700

    refactorize mailer

--- lib/straw_hat_mailer/email.ex
+++ lib/straw_hat_mailer/email.ex
@@ -4,44 +4,55 @@ defmodule StrawHat.Mailer.Email do
   alias Mustache
   alias StrawHat.Mailer.Template
 
-  defdelegate to(email, recipient), to: Swoosh.Email
-  defdelegate from(email, from), to: Swoosh.Email
+  def new_email(email) do
+    case email[:template] do
+      nil -> new(email)
+      name -> 
+        options = get_options(email)
+        email
+        |> Enum.filter(fn({key, _}) -> not key in [:template, :options] end)
+        |> create_email_with_template(name, options)
+    end
+  end
 
-  def template(name, options) do
-    case Template.get_template_by_name(name) do
-      {:error, _reason} -> new()
+  def create_email_with_template(email, template_name, options) do
+    case Template.get_template_by_name(template_name) do
+      {:error, _reason} -> new(email)
       {:ok, template} ->
-        new()
-        |> add_subject(template, options)
-        |> add_html(template, options)
-        |> add_text(template, options)
+        email
+        |> add_subject(template.subject, options)
+        |> add_html_body(template.html_body, options)
+        |> add_text_body(template.text_body, options)
+        |> new()
     end
   end
 
-  defp add_subject(email, template, options) do
-    case template.subject do
-      nil -> email
-      subject ->
-        subject = Mustache.render(subject, options)
-        subject(email, subject)
-    end
+  defdelegate send(email), to: StrawHat.Mailer, as: :deliver
+
+  def send_later(email) do
+    Task.start(fn ->
+      send(email)
+    end)
   end
 
-  defp add_html(email, template, options) do
-    case template.html_body do
-      nil  -> email
-      html ->
-        html = Mustache.render(html, options)
-        html_body(email, html)
-    end
+  defp get_options(email) do
+    with nil <- email[:options], do: %{}
   end
 
-  defp add_text(email, template, options) do
-    case template.text_body do
-      nil  -> email
-      text ->
-        text = Mustache.render(text, options)
-        text_body(email, text)
-    end
+  defp add_subject(email, subject, options) do
+    subject = render(subject, options)
+    Enum.concat(email, [subject: subject])
   end
+
+  defp add_html_body(email, html_body, options) do
+    html_body = render(html_body, options)
+    Enum.concat(email, [html_body: html_body])
+  end
+
+  defp add_text_body(email, text_body, options) do
+    text_body = render(text_body, options)
+    Enum.concat(email, [text_body: text_body])
+  end
+
+  defp render(template, options), do: Mustache.render(template, options)
 end
