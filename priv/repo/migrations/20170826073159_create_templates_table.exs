75a2c4cdd732ea7b01b59ab5127e9089ea785364
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-09-17 22:44:14 -0700

    add field subject to migrations

--- priv/repo/migrations/20170826073159_create_templates_table.exs
+++ priv/repo/migrations/20170826073159_create_templates_table.exs
@@ -5,6 +5,7 @@ defmodule StrawHat.Mailer.Repo.Migrations.CreateTemplatesTable do
     create table(:templates) do
       add(:name, :string, null: false)
       add(:title, :string, null: false)
+      add(:subject, :string, null: false)
       add(:owner_id, :string, null: false)
       add(:privacy, :string, default: "private")
       add(:text_body, :string)
