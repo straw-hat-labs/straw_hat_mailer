31a143ea8276a4128fa0674e365d1c3fe11ae599
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-10-15 15:01:36 -0400

    add docs

--- lib/straw_hat_mailer/template.ex
+++ lib/straw_hat_mailer/template.ex
@@ -16,9 +16,15 @@ defmodule StrawHat.Mailer.Template do
     html_body: String.t,
   }
 
+  @doc """
+  Get the list of templates.
+  """
   @spec get_templates(Scrivener.Config.t) :: Scrivener.Page.t
   def get_templates(pagination \\ []), do: Repo.paginate(Template, pagination)
 
+  @doc """
+  Create a template.
+  """
   @spec create_template(template_attrs) :: {:ok, Template.t} | {:error, Ecto.Changeset.t}
   def create_template(template_attrs) do
     %Template{}
@@ -26,6 +32,9 @@ defmodule StrawHat.Mailer.Template do
     |> Repo.insert()
   end
 
+  @doc """
+  Update a template.
+  """
   @spec update_template(Template.t, template_attrs) :: {:ok, Template.t} | {:error, Ecto.Changeset.t}
   def update_template(%Template{} = template, template_attrs) do
     template
@@ -33,9 +42,15 @@ defmodule StrawHat.Mailer.Template do
     |> Repo.update()
   end
 
+  @doc """
+  Destroy a template.
+  """
   @spec destroy_template(Template.t) :: {:ok, Template.t} | {:error, Ecto.Changeset.t}
   def destroy_template(%Template{} = template), do: Repo.delete(template)
 
+  @doc """
+  Get a template by `id`.
+  """
   @spec find_template(String.t) :: {:ok, Template.t} | {:error, Error.t}
   def find_template(template_id) do
     case get_template(template_id) do
@@ -46,9 +61,15 @@ defmodule StrawHat.Mailer.Template do
     end
   end
 
+  @doc """
+  Get a template by `id`.
+  """
   @spec get_template(String.t) :: Ecto.Schema.t | nil | no_return
   def get_template(template_id), do: Repo.get(Template, template_id)
 
+  @doc """
+  Get a template by `name`.
+  """
   @spec get_template_by_name(String.t) :: {:ok, Template.t} | {:error, Ecto.Changeset.t}
   def get_template_by_name(template_name) do
     clauses = [name: template_name]
