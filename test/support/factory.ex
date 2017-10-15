3da17fbb942e0342c274260cd712cee3eff203f5
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-10-15 16:57:20 -0400

    fix factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,15 +1,19 @@
 defmodule StrawHat.Mailer.Test.Factory do
   use ExMachina.Ecto, repo: StrawHat.Mailer.Repo
 
-  alias StrawHat.Mailer.Schema.{
-    Template}
+  alias StrawHat.Mailer.Schema.{Template}
 
   def template_factory do
+    privacy =
+      StrawHat.Mailer.Template.Privacy.values()
+      |> Enum.take_random(1)
+      |> List.first()
+
     %Template{
       name: Faker.Pokemon.name(),
       title: Faker.Name.title(),
       owner_id: Faker.String.base64(),
-      privacy: "private",
+      privacy: privacy,
       subject: "Milka Suberast",
       html_body: "Welcome {{name}}, enjoy a good reputation <br> <b>Become </b> our client number <i>{{number}}</i>, enjoy the service."}
   end
