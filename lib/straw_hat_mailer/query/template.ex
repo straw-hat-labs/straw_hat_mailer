7b619e3e0f8bba15eb6c19ad21c72f4cd2033937
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-11-28 00:02:14 -0800

    fix query

--- lib/straw_hat_mailer/query/template.ex
+++ lib/straw_hat_mailer/query/template.ex
@@ -2,8 +2,8 @@ defmodule StrawHat.Mailer.Query.TemplateQuery do
   import Ecto.Query, only: [from: 2]
 
   def by_name(query, name) do
-    from locker in query,
-      where: locker.name == ^name,
+    from template in query,
+      where: template.name == ^name,
        preload: :partial
   end
 end
