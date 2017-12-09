f49004ce73683bfe3cb38c3f16dc8630d7a8c7f0
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-12-09 11:38:45 -0800

    cleaning up factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -4,10 +4,7 @@ defmodule StrawHat.Mailer.Test.Factory do
   alias StrawHat.Mailer.Schema.{Template, Partial}
 
   def template_factory do
-    privacy =
-      StrawHat.Mailer.Template.Privacy.values()
-      |> Enum.take_random(1)
-      |> List.first()
+    privacy = get_privacy()
 
     %Template{
       name: Faker.Pokemon.name(),
@@ -17,22 +14,23 @@ defmodule StrawHat.Mailer.Test.Factory do
       subject: "Milka Suberast",
       pre_header: "Behold then sings my soul",
       html_body: "Welcome, enjoy a good reputation <br> <b>Become </b> our client number <i>{{number}}</i>, enjoy the service.",
-      text_body: "Text with name, plain and my number is {{number}}",
-      partial: build(:partial)}
+      text_body: "Text with name, plain and my number is {{number}}"}
   end
 
   def partial_factory do
-    privacy =
-      StrawHat.Mailer.Template.Privacy.values()
-      |> Enum.take_random(1)
-      |> List.first()
+    privacy = get_privacy()
 
     %Partial{
-      html_header: "{{company}} the best in the market!",
-      html_footer: "Located in: {{address}}",
-      text_header: "{{company}} the best in the market!",
-      text_footer: "Located in: {{address}}",
+      key: Faker.String.base64(),
+      html: "<b>Located in:</b> {{address}}",
+      text: "Located in: {{address}}",
       privacy: privacy,
       owner_id: Faker.String.base64()}
+  end
+
+  defp get_privacy() do
+    StrawHat.Mailer.Template.Privacy.values()
+    |> Enum.take_random(1)
+    |> List.first()
   end
 end
