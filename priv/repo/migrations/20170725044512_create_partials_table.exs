155361b2820d32cf5b1da8b579931a8bfa7e948c
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-12-09 11:39:34 -0800

    refactorize partial table

--- priv/repo/migrations/20170725044512_create_partials_table.exs
+++ priv/repo/migrations/20170725044512_create_partials_table.exs
@@ -3,10 +3,9 @@ defmodule StrawHat.Mailer.Repo.Migrations.CreatePartialsTable do
 
   def change do
     create table(:partials) do
-      add(:html_header, :text, null: false)
-      add(:html_footer, :text, null: false)
-      add(:text_header, :text, null: false)
-      add(:text_footer, :text, null: false)
+      add(:key, :string, null: false)
+      add(:html, :text)
+      add(:text, :text)
       add(:privacy, :string, default: "private")
       add(:owner_id, :string, null: false)
     end
