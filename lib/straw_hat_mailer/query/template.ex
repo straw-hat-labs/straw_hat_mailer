55358d33ba16bbdd70c39dd54d9db191e2ed3867
Osley Zorrilla Rivera <ozorrilla87@gmail.com>
2017-12-10 21:14:27 -0800

    add type spec

--- lib/straw_hat_mailer/query/template.ex
+++ lib/straw_hat_mailer/query/template.ex
@@ -3,6 +3,7 @@ defmodule StrawHat.Mailer.Query.TemplateQuery do
 
   import Ecto.Query, only: [from: 2]
 
+  @spec by_name(Template.t, String.t) :: Ecto.Query.t
   def by_name(query, name) do
     from template in query,
       where: template.name == ^name,
--- lib/straw_hat_mailer/query/template_partial.ex
+++ lib/straw_hat_mailer/query/template_partial.ex
@@ -3,6 +3,7 @@ defmodule StrawHat.Mailer.Query.TemplatePartialQuery do
 
   import Ecto.Query, only: [from: 2]
 
+  @spec get_by(TemplatePartial.t, Integer.t, [Integer.t]) :: Ecto.Query.t
   def get_by(query, template_id, partial_ids) do
     from template_partial in query,
      where: template_partial.template_id == ^template_id,
