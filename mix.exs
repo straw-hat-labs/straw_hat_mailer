0bfbb89a4f52f09d58b2eb03e9d08013396c7a52
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-03-23 11:32:07 -0700

    fix test command

--- mix.exs
+++ mix.exs
@@ -76,7 +76,7 @@ defmodule StrawHat.Mailer.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test --trace"]
     ]
   end
 
