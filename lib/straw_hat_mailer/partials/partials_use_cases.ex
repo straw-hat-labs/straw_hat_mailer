5699bd4b4eabd4e21ef49616d4ae37e7925b730d
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-02-03 18:09:07 -0800

    Fix partial test cases

--- lib/straw_hat_mailer/partials/partials_use_cases.ex
+++ lib/straw_hat_mailer/partials/partials_use_cases.ex
@@ -1,76 +1,60 @@
 defmodule StrawHat.Mailer.Partials do
   @moduledoc """
-  Defines functionality for partial management.
+  Partials management use cases.
 
-  A Partial is just a chunk of email content that you could reuse
-  cross multiple emails.
+  A Partial is just a chunk of email content that you could reuse cross multiple
+  emails. This is useful when you want to share sections between email
+  templates.
   """
 
-  use StrawHat.Mailer.Interactor
+  import Ecto.Query
+  alias StrawHat.{Error, Response}
   alias StrawHat.Mailer.Partial
 
-  @doc """
-  Returns the list of partials.
-  """
-  @since "1.0.0"
-  @spec get_partials(Scrivener.Config.t()) :: Scrivener.Page.t()
-  def get_partials(pagination \\ []), do: Repo.paginate(Partial, pagination)
+  @spec get_partials(Ecto.Repo.t(), Scrivener.Config.t()) :: Scrivener.Page.t()
+  def get_partials(repo, pagination \\ []) do
+    Scrivener.paginate(Partial, Scrivener.Config.new(repo, [], pagination))
+  end
 
-  @doc """
-  Creates a partial.
-  """
-  @since "1.0.0"
-  @spec create_partial(Partial.partial_attrs()) ::
-          {:ok, Partial.t()} | {:error, Ecto.Changeset.t()}
-  def create_partial(partial_attrs) do
+  @spec create_partial(Ecto.Repo.t(), Partial.partial_attrs()) ::
+          Response.t(Partial.t(), Ecto.Changeset.t())
+  def create_partial(repo, partial_attrs) do
     %Partial{}
     |> Partial.changeset(partial_attrs)
-    |> Repo.insert()
+    |> repo.insert()
+    |> Response.from_value()
   end
 
-  @doc """
-  Updates a partial.
-  """
-  @since "1.0.0"
-  @spec update_partial(Partial.t(), Partial.partial_attrs()) ::
-          {:ok, Partial.t()} | {:error, Ecto.Changeset.t()}
-  def update_partial(%Partial{} = partial, partial_attrs) do
+  @spec update_partial(Ecto.Repo.t(), Partial.t(), Partial.partial_attrs()) ::
+          Response.t(Partial.t(), Ecto.Changeset.t())
+  def update_partial(repo, %Partial{} = partial, partial_attrs) do
     partial
     |> Partial.changeset(partial_attrs)
-    |> Repo.update()
+    |> repo.update()
+    |> Response.from_value()
   end
 
-  @doc """
-  Destroys a partial.
-  """
-  @since "1.0.0"
-  @spec destroy_partial(Partial.t()) :: {:ok, Partial.t()} | {:error, Ecto.Changeset.t()}
-  def destroy_partial(%Partial{} = partial), do: Repo.delete(partial)
+  @spec destroy_partial(Ecto.Repo.t(), Partial.t()) :: Response.t(Partial.t(), Ecto.Changeset.t())
+  def destroy_partial(repo, %Partial{} = partial) do
+    partial
+    |> repo.delete()
+    |> Response.from_value()
+  end
 
-  @doc """
-  Gets a partial by `id`.
-  """
-  @since "1.0.0"
-  @spec find_partial(String.t()) :: {:ok, Partial.t()} | {:error, Error.t()}
-  def find_partial(partial_id) do
-    partial_id
-    |> get_partial()
+  @spec find_partial(Ecto.Repo.t(), String.t()) :: Response.t(Partial.t(), Ecto.Changeset.t())
+  def find_partial(repo, partial_id) do
+    repo
+    |> get_partial(partial_id)
     |> Response.from_value(
       Error.new("straw_hat_mailer.partial.not_found", metadata: [partial_id: partial_id])
     )
   end
 
-  @doc """
-  Gets a partial by `id`.
-  """
-  @since "1.0.0"
-  @spec get_partial(String.t()) :: Partial.t() | nil | no_return
-  def get_partial(partial_id), do: Repo.get(Partial, partial_id)
+  @spec get_partial(Ecto.Repo.t(), String.t()) :: Partial.t() | nil | no_return
+  def get_partial(repo, partial_id) do
+    repo.get(Partial, partial_id)
+  end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking partial changes.
-  """
-  @since "1.0.0"
   @spec change_partial(Partial.t()) :: Ecto.Changeset.t()
   def change_partial(%Partial{} = partial) do
     Partial.changeset(partial, %{})
@@ -80,16 +64,11 @@ defmodule StrawHat.Mailer.Partials do
   Returns a list of partials that belongs to the `owner_id`.
   """
   @since "1.0.0"
-  @spec get_owner_partials(String.t(), Scrivener.Config.t()) :: Scrivener.Page.t()
-  def get_owner_partials(owner_id, pagination \\ []) do
-    owner_id
-    |> partials_by_owner()
-    |> Repo.paginate(pagination)
-  end
-
-  @since "1.0.0"
-  @spec partials_by_owner(String.t()) :: Ecto.Query.t()
-  defp partials_by_owner(owner_id) do
-    from(partial in Partial, where: partial.owner_id == ^owner_id)
+  @spec get_owner_partials(Ecto.Repo.t(), String.t(), Scrivener.Config.t()) :: Scrivener.Page.t()
+  def get_owner_partials(repo, owner_id, pagination \\ []) do
+    Partial
+    |> select([partial], partial)
+    |> where(owner_id: ^owner_id)
+    |> Scrivener.paginate(Scrivener.Config.new(repo, [], pagination))
   end
 end
--- test/straw_hat_mailer/partials_test.exs
+++ test/straw_hat_mailer/partials_test.exs
@@ -7,14 +7,14 @@ defmodule StrawHat.Mailer.PartialsTests do
     test "with a valid owner id returns the list of partials" do
       owner_id = "user:123"
       insert_list(3, :partial, %{owner_id: owner_id})
-      partial_pagination = Partials.get_owner_partials(owner_id)
+      partial_pagination = Partials.get_owner_partials(Repo, owner_id)
 
       assert length(partial_pagination.entries) == 3
     end
 
     test "with an invalid owner returns an empty list" do
       insert_list(3, :partial, %{owner_id: "user:123"})
-      partial_pagination = Partials.get_owner_partials("admin:1234")
+      partial_pagination = Partials.get_owner_partials(Repo, "admin:1234")
 
       assert partial_pagination.entries == []
     end
@@ -22,33 +22,34 @@ defmodule StrawHat.Mailer.PartialsTests do
 
   test "get_partial/1 with a valid id finds the partial" do
     partial = insert(:partial)
-    assert Partials.get_partial(partial.id) != nil
+    assert Partials.get_partial(Repo,partial.id) != nil
   end
 
   test "get_partial/1 with a invalid id returns an error" do
-    assert {:error, _reason} = Ecto.UUID.generate() |> Partials.find_partial()
+    partial_id = Ecto.UUID.generate()
+    assert {:error, _reason} = Partials.find_partial(Repo, partial_id)
   end
 
   test "get_partials/1 returns a list of partials" do
     insert_list(3, :partial)
-    partial = Partials.get_partials(%{page: 2, page_size: 2})
+    partial = Partials.get_partials(Repo, %{page: 2, page_size: 2})
     assert length(partial.entries) == 1
   end
 
   test "create partial" do
     params = params_for(:partial)
-    assert {:ok, _partial} = Partials.create_partial(params)
+    assert {:ok, _partial} = Partials.create_partial(Repo, params)
   end
 
   test "update_partial/2 with valid data updates the partial" do
     partial = insert(:partial)
-    {:ok, partial} = Partials.update_partial(partial, %{text: "Real location {{location}}"})
+    {:ok, partial} = Partials.update_partial(Repo, partial, %{text: "Real location {{location}}"})
     assert partial.text == "Real location {{location}}"
   end
 
   test "delete_partial/1 with valid partial deletes the partial" do
     partial = insert(:partial)
-    assert {:ok, _} = Partials.destroy_partial(partial)
+    assert {:ok, _} = Partials.destroy_partial(Repo, partial)
   end
 
   test "change_partial/1 returns a partial changeset" do
