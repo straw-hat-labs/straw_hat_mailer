a67b265fbff6ef9309ac00e5b8611807b752d32e
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-02-06 21:38:49 -0800

    Improve docs

--- lib/straw_hat_mailer.ex
+++ lib/straw_hat_mailer.ex
@@ -1,13 +1,20 @@
 defmodule StrawHat.Mailer do
   @moduledoc """
-  Check `Swoosh.Mailer` documentation for learn more about this module.
+  This module is an extension of `Swoosh.Mailer`, please read more about
+  `Swoosh.Mailer` documentation.
+
+  You can swap the adapter doing the following config:
+
+      config :straw_hat_mailer, StrawHat.Mailer,
+        adapter: Swoosh.Adapters.Sendgrid,
+        api_key: "SG.x.x"
   """
   use Swoosh.Mailer, otp_app: :straw_hat_mailer
 
   @typedoc """
   The identifier of the owner. We recommend to use combinations
-  of `system + resource id`. For example: `"system_name:resource_id"` or any other
-  combination. The reason behind is that if you use just some resource id,
+  of `system + resource id`. For example: `"system_name:resource_id"` or any
+  other combination. The reason behind is that if you use just some resource id,
   example just `"1"`, you can't use more than one resource that owns the
   template with the same `id`.
   """
@@ -15,8 +22,6 @@ defmodule StrawHat.Mailer do
 
   @doc """
   Send an email asynchronous.
-
-  It use `StrawHat.Mailer.deliver/1` inside `Task.start/1`.
   """
   @spec deliver_later(Swoosh.Email.t(), keyword) :: {:ok, pid}
   def deliver_later(email, config \\ []) do
